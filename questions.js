0cab97477de61aefa66f567d87314559ea4440d5
Niki Fereidooni <nikifereidooni@gmail.com>
2020-09-28 20:33:49 -0400

    question.html has been combined with index.html for better functionality. Javscript files created and linked accordingly.

---
+++ questions.js
@@ -0,0 +1,27 @@
+var question = [
+    {
+        qTitle: "Commonly use data types DO NOT Include:",
+        qChoices: ["Strings", "Booleans", "Alerts", "Numbers"],
+        qAnswer: "Alerts"
+    },
+    {
+        qTitle: "The condition in an if/else statement is enclosed within:",
+        qChoices: ["Quotes", "Curly brackets", "Parentheses", "Square brackets"],
+        qAnswer: "Parentheses"
+    },
+    {
+        qTitle: "Arrays in Javascript can be used to store:",
+        qChoices: ["Numbers and Strings", "Other arrays", "Booleans", "All of the above"],
+        qAnswer: "All of the above"
+    },
+    {
+        qTitle: "String values must be enclosed within:",
+        qChoices: ["Commas", "Curly brackets", "Quotes", "Parentheses"],
+        qAnswer: "Curly brackets"
+    },
+    {
+        qTitle: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        qChoices: ["Javascript", "Terminal / Bash", "For loops", "Console log"],
+        qAnswer: "For loops"
+    },
+];
